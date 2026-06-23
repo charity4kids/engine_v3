b101406b2662d3460b056f5a4a5e308d4ada8e14
charity4kids <1butterflyup@gmail.com>
2026-06-23 11:51:38 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,8 @@
-module.exports = {
-  query: async () => {
-    console.log("DB placeholder");
-  }
-};
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
+});
+
+module.exports = pool;
